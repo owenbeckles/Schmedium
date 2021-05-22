c9ec8da8ab98877f8ed7d30edb9c959755477af1
becklesoweno@gmail.com <becklesoweno@gmail.com>
2021-05-22 19:33:14 -0400

    Signup Action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { rootReducer } from './store/index'
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
